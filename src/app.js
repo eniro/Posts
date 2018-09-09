cce6b50e777016122f25ab3920b5f7e7952e03eb
Iro Enezli <enezliro@gmail.com>
2018-09-09 21:51:24 +0300

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -19,24 +19,50 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 // Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Submit Post
 function submitPost() {
     const title = document.querySelector('#title').value;
     const body = document.querySelector('#body').value;
+    const id = document.querySelector('#id').value;
 
     const data = {
         title,
         body
     }
-
-    // Create Post
-    http.post('http://localhost:3000/posts', data)
-        .then(data => {
-            ui.showAlert('Post added', 'alert alert-success');
-            ui.clearFields();
-            getPosts();
+    
+    // Validate input
+    if (title === '' || body === '') {
+        ui.showAlert('Please fill in all fields', 'alert alert-danger');
+    } else {
+        if (id === '') {
+            // Create Post
+            http.post('http://localhost:3000/posts', data)
+                .then(data => {
+                ui.showAlert('Post added', 'alert alert-success');
+                ui.clearFields();
+                getPosts();
         })
         .catch(err => console.log(err));
+        } else {
+            // Update Post
+            http.put(`http://localhost:3000/posts/${id}`, data)
+            .then(data => {
+            ui.showAlert('Post updated', 'alert alert-success');
+            ui.changeFormState('add');
+            getPosts();
+    })
+    .catch(err => console.log(err));
+
+        }
+        
+    
+        
+    }
+
+    
 }
 
 // Delete Post
@@ -73,5 +99,13 @@ function enableEdit(e) {
         ui.fillForm(data);
     }
 
+    e.preventDefault();
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+    if(e.target.classList.contains('post-cancel')) {
+        ui.changeFormState('add');
+    }
     e.preventDefault();
 }
